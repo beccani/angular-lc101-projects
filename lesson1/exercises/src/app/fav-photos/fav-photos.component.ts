7c5f212c536aa3379529f913c9d949099077fc44
Becca Johnson <beccanjohnson@gmail.com>
2020-12-16 19:37:32 -0600

    first angular commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some Awesome Pictures';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://mixkit.imgix.net/art/preview/mixkit-two-flamingos-standing-in-the-water-438-square-large.png';
+  image3 = 'https://mixkit.imgix.net/art/preview/mixkit-starry-night-sky-over-hills-and-water-85-square-large.png';
 
   constructor() { }
 
