0d88cd60716636d2c4a970d1c7b7948fbc440f75
Becca Johnson <beccanjohnson@gmail.com>
2020-12-20 18:46:08 -0600

    input practice completed

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
    templateUrl: './movie-list.component.html',
    styleUrls: ['./movie-list.component.css']
 })
+
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
 
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie (newTitle:string) {
+      if (newTitle !== "" && !this.movies.includes(newTitle)) {
+         this.movies.push(newTitle);
+      }         
    }
 }
